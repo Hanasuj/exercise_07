7288c971101c5fc637b432e0c736151c2974788f
Hanasuj <124556657+Hanasuj@users.noreply.github.com>
2025-11-01 21:15:46 +0100

    Add files via upload

--- cv7.R
+++ cv7.R
@@ -1,31 +1,32 @@
 rm(list=ls())
-setwd("V:/MPA/MPAPRG/exercise_07")
+setwd("E:/asdghjk/skola/VUT Brno/inzinier/4rocnik/PRG")
 
 # Libraries
 library(Biostrings)
 
 
 ## Task 1
-Score <- function(s, sequences, l){
-  motifs <- character(length(sequences))
-  
-  for (i in 1:length(sequences)) {
-    motifs[i] <- substr(as.character(sequences[i]), s[i], s[i] + l - 1)
-    print(motifs)
+Score <- function(s, DNA, l){
+  alignment_matrix <- c()
+  for (i in 1:length(s)){
+    seq <- subseq(DNA[[i]], start = s[i], width = l)
+    alignment_matrix <- c(alignment_matrix, seq)
   }
-  motifs <- as.matrix(motifs)
+  alignment_matrix <- DNAStringSet(alignment_matrix)
+  
+  frequency_profile <- consensusMatrix(alignment_matrix)
+  
+  consensus_string <- c()
   
   score <- 0
-  
-  for (column in 1:l){
-    occurences <- table(motifs[,column])
-    score = score + as.numeric(occurences[which.max(occurences)])
-    
+  for (i in 1:l){
+    score <- score + max(frequency_profile[,i])
   }
+  
   print(score)
   return(score)
 }
-
+  
 sequences <- DNAStringSet(c("ATCCGTA", "GTGCATA", "AAGCGTA", "ATGCGTG"))
 
 print(Score(c(1,1,1,1), sequences, 7))
@@ -67,16 +68,16 @@ print(BFMotifSearch(DNA, 4, 8, 3))
 NextVertex <- function(s, i, t, k){
   if (i < t){
     s[i + 1] <- 1
-    return(s, i+1)
+    return(list(s, i+1))
   }else{
     for(j in t:1){
       if(s[j] < k){
         s[j] <- s[j] + 1
-        return(s, j)
+        return(list(s, j))
       }
     }
   }
-  return(s, 0)
+  return(list(s, 0))
 }
 
 ## Task 5
@@ -84,46 +85,45 @@ ByPass <- function(s, i, t, k){
   for (j in i:1){
     if (s[j] < k){
       s[j] <- s[j] + 1
-      return(s, j)
+      return(list(s, j))
     }
   }
-  return(s, 0)
+  return(list(s, 0))
 }
 
 ## Task 6
-# BBMotifSearch<- function(DNA, t, n, l){
-#   s <- rep(1, t)
-#   bestScore <- 0
-#   i <- 1
-#   while (i > 0){
-#     if(i < t){
-#       optimisticScore <- Score(s, i, DNA, l) + (t - i) * l
-#       if (optimisticScore < bestScore){
-#         (s, i) <- ByPass(s, i, t, n - l + 1)
-#       }else{
-#         (s, i) <- NextVertex(s, i, t, n - l + 1)
-#       }
-#     }else if (Score(s, t, DNA, l) > bestScore){
-#       bestScore <- Score(s, t, DNA, l)
-#       bestMotif <- s
-#       (s, i) <- NextVertex(s, i, t, n − l + 1)
-#     }else{
-#       
-#     }
-#   }
-#   return(bestMotif)
-# }
+BBMotifSearch<- function(DNA, t, n, l){
+  s <- rep(1, t)
+  bestScore <- 0
+  i <- 1
+  while (i > 0){
+    if(i < t){
+      optimisticScore <- Score(s, DNA, l) + (t - i) * l
+      if (optimisticScore < bestScore){
+        bypass <- ByPass(s, i, t, n - l + 1)
+        s <- bypass[[1]]
+        i <- bypass[[2]]
+     }else{
+        next_vertex <- NextVertex(s, i, t, n - l + 1)
+        s <- next_vertex[[1]]
+        i <- next_vertex[[2]]
+      }
+    }else if (Score(s, DNA, l) > bestScore){
+      bestScore <- Score(s, DNA, l)
+      bestMotif <- s
+      next_vertex <- NextVertex(s, i, t, n - l + 1)
+      s <- next_vertex[[1]]
+      i <- next_vertex[[2]]
+    }else{
+      
+    }
+  }
+  return(bestMotif)
+}
 
+result <- BBMotifSearch(DNA, 4, 8, 3)
 
-
-
-
-
-
-
-
-
-
+print(result)
 
 
 
